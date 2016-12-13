c3365989662c85defe3b4c5f4a47c8cfcffc2250
Rob Hitt <robhitt@gmail.com>
2016-12-13 15:10:12 -0500

    done

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,9 +3,16 @@ class Song < ActiveRecord::Base
   belongs_to :genre
 
   def get_genre_name
+    self.genre.name
   end
 
   def drake_made_this
     # when this method is called it should assign the song's artist to Drake
+    # drake = Artist.create(name: "Drake")
+    # self.artist = drake
+
+    drake = Artist.new(name: "Drake")
+    drake.songs << self
+    drake
   end
 end
